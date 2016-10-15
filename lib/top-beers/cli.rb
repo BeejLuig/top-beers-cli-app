4a4571a8129fe615807077b36083365e0031b117
BeejLuig <bjcantlupe@gmail.com>
2016-10-15 15:38:06 -0400

    Updated #menu in cli.rb

--- lib/top-beers/cli.rb
+++ lib/top-beers/cli.rb
@@ -21,17 +21,22 @@ class TopBeers::CLI
   end
 
   def menu
-    puts "Enter the number of the beer you want more information about or type exit to leave the program"
     input = nil
     while input != "exit"
+      puts "Enter the number of the beer you want more information about or type list to see the beer list again or type exit to leave the program"
       input = gets.strip.downcase
       case input
       when "1"
         puts "More info on beer 1..."
       when "2"
-        puts "More info on beer 1..."
+        puts "More info on beer 2..."
       when "3"
-        puts "More info on beer 1..."
+        puts "More info on beer 3..."
+      when "list"
+        list_beers
+      else
+        puts "Not sure what you want. Type list or exit."
+      end
     end
   end
 
