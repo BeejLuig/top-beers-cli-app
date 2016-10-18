4adb05066abe9d789ff9901a4c205ba99bcb5402
BeejLuig <bjcantlupe@gmail.com>
2016-10-18 12:32:04 -0400

    added TopBeers::Style class
    based off of TopBeers::Brewery class

---
+++ lib/top-beers/style.rb
@@ -0,0 +1,34 @@
+class TopBeers::Style
+  attr_accessor :beers
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name)
+    @beers = []
+    @name = name
+    @@all << self
+  end
+
+  def self.find_or_create_by_name(name)
+    style = @@all.detect {|b| b.name.downcase == name.downcase}
+    if style.nil?
+      style = self.new(name)
+    end
+    style
+  end
+
+  def show_beers
+    puts "\n"+"-"*"#{@name}'s Beers in the Top 250".length
+    puts "#{@name}'s Beers in the Top 250"
+    puts "-"*"#{@name}'s Beers in the Top 250".length
+    @beers.each.with_index(1) do |beer, i|
+      puts "#{i}. #{beer.name} - #{beer.style}"
+    end
+    puts "\n"
+  end
+
+  def self.all
+    @@all
+  end
+end
